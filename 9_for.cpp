6d9b452ec44ad73dd3421329b3d787cbda604c2e
boltPC1 <info@schoolbolt.ru>
2019-11-08 19:01:06 +0500

    Сообщение

--- 9_for.cpp
+++ 9_for.cpp
@@ -67,35 +67,37 @@ int main()
 		std::cout << std::endl;
 	}
 	
-	for (int i = n,j = 2; i > 0; i--) {
-		for (int i = j; i > 0 && i <= n; i--) {
-			std::cout << "*";
-		}j++;
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+            std::cout << "*";
+        }
 		std::cout << std::endl;
 	}
 	
 	
 	std::cout << "task 9" << std::endl;
-	
-	for (int i = n; i > 0; i--) {
-		for (int j = n; i < j; j--)
-		{
-			std::cout << "p";
-		}
-		for (int j = 0; i > j; j++) {
-			std::cout << "*";
-		}
-		for (int j = 1; i > j; j++) {
-			std::cout << "*";
-		}
-		std::cout << std::endl;
-	}
-	for (int i = n; i > 0; i--) {
-		for (int j = 2; j < i; j++) {
-			std::cout << "p";
-		}
-		std::cout << std::endl;
-	}
+    
+    for (int i = n; i > 0; i-= 2) {
+        for (int j = 0; j < (n - i) / 2; j++) {
+            std::cout << " ";
+        }
+        for (int j = 0; j < i; j++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    for (int i = 0; i <= n; i+= 2) {
+        for (int j = n-i; j > (n - i) / 2; j--) {
+            std::cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+
+
 	std::cout << std::endl;
 	std::cin.get(); std::cin.get();
 	return 0;
