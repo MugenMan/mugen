60cd18c69c7a2b4f82ba4b3393b71894bf9e9986
MugenMan <powerheller@gmail.com>
2019-11-21 17:26:54 +0500

    while

--- 13_while.cpp
+++ 13_while.cpp
@@ -4,15 +4,17 @@
 
 int main()
 {
-	int n,r=1;
+	int n,r=0;
     std::cout << "enter n" << std::endl;
 	std::cin >> n;
-    while(n != 0){
-    r=r*n;
+
+    while( (r < 100) && (n < 100) ){
+    r=r+n;
     std::cout << "enter n" << std::endl;
 	std::cin >> n;
 }
-    std::cout << "product of numbers = " << r << std::endl;
+    r=r+n;
+    std::cout << "sum of numbers = " << r << std::endl;
     std::cout << std::endl;
 	std::cin.get(); std::cin.get();
 	return 0;
--- 14_while.cpp
+++ 14_while.cpp
@@ -4,15 +4,16 @@
 
 int main()
 {
-	int n,r=1;
+	int n,r=0;
     std::cout << "enter n" << std::endl;
 	std::cin >> n;
     while(n != 0){
-    r=r*n;
-    std::cout << "enter n" << std::endl;
-	std::cin >> n;
+    r=r+n%10;
+    n=n/10;
+    std::cout << "n = " << n << std::endl;
 }
-    std::cout << "product of numbers = " << r << std::endl;
+    std::cout << "sum of digits in number = " << r << std::endl;
+    
     std::cout << std::endl;
 	std::cin.get(); std::cin.get();
 	return 0;
