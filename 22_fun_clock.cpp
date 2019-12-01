973bf3d41592698b2367442ae1c869ae56fabd78
MugenMan <powerheller@gmail.com>
2019-12-01 15:26:25 +0500

    add_22

--- 22_fun_clock.cpp
+++ 22_fun_clock.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include <math.h>
 
-void fiba(int x){
+void fiba(int x, int t=0){
 
     for (int i = x; i > 0; i -= 2) {
-		for (int j = 0; j < (x - i) / 2; j++) {
+		for (int j = 0-t; j < (x - i) / 2; j++) {
 			std::cout << " ";
 		}
 		for (int j = 0; j < i; j++) {
@@ -14,7 +14,7 @@ void fiba(int x){
 		std::cout << std::endl;
 	}
 	for (int i = 2; i < x; i += 2) {
-		for (int j = 0; j < (x - i) / 2; j++) {
+		for (int j = 0-t; j < (x - i) / 2; j++) {
 			std::cout << " ";
 		}
 		for (int j = 0; j <= i; j++) {
@@ -32,7 +32,10 @@ int main()
     std::cout << "enter N: " << std::endl;
 	std::cin >> N;
 
-    std::cout << "fib = " << otv << std::endl;
+	fiba(N,0);
+	fiba(N-2,+1);
+	fiba(N-4,+2);
+	
    
         return 0;
 }
