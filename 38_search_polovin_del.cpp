6925c1a2f6ebbb812df4bbfda4898a5e41325565
MugenMan <powerheller@gmail.com>
2020-01-22 00:37:53 +0500

    edit38

--- 38_search_polovin_del.cpp
+++ 38_search_polovin_del.cpp
@@ -28,47 +28,57 @@ int N, n;
             if (arr[s] < arr[a]){
                 a = s;
             }
-            //std::cout << "arrs=" << arr[s] <<std::endl;
+            
             }
-            //std::cout << "arrA=" << arr[a] <<std::endl;
+            
             
             b=arr[k];
             arr[k] = arr[a];
             arr[a] = b;
 
-            //std::cout << "arrk=" << arr[k] <<std::endl;
-            //std::cout << "arrc=" << arr[a] <<std::endl;
-        //     for (int j = 0; j < arr.size(); j++){
-        // std::cout << j << ". " << arr[j] << std::endl;
-        //}
     }
     std::cout << std::endl;
     for (int j = 0; j < arr.size(); j++){
         std::cout << j << ". " << arr[j] << std::endl;
     }
 
-    std::cout << "Enter number n: " << std::flush;
+    std::cout << "Enter number n: " << std::endl;
     std::cin >> n; 
 
-    int r = N/2, z;
-    
-    for (int k = 0; k < N/2; k++){
-        if (n == arr[r]){
-            z = arr[r];
+    int start=0, finish=N, f;
+    for (int k= 0; k < N; k++){
+    	f= (start+finish)/2;
+        if (arr[f] == n){
+            std::cout<< n <<std::endl;
         }
-        if (n > arr[r]){
-            z = arr[r+((N-r)/2)];
-
+        else if (arr[f]<n){
+            start= f+1;
         }
-        if (n < arr[r]){
-            z = arr[r-((N-r)/2)];
+        else if (arr[f]>n){
+            finish= f-1;
         }
-        
     }
-        std::cout << "arrr=" << arr[r] <<std::endl;
-        std::cout << "r=" << r <<std::endl;
-        std::cout << "z=" << z <<std::endl;
+    if (arr[f] == n){
+        std::cout << "index= " << f <<std::endl;}
+    else {std::cout<< "not found" <<std::endl;}
+        
+        
+        //std::cout << "r=" << r <<std::endl;
+    // int r = N/2, z;
+    
+    // for (int k = 0; k < N/2; k++){
+    //     if (n == arr[r]){
+    //         z = arr[r];
+    //     }
+    //     if (n > arr[r]){
+    //         z = arr[r+((N-r)/2)];
 
-    std::cout << std::endl;
+    //     }
+    //     if (n < arr[r]){
+    //         z = arr[r-((N-r)/2)];
+    //     }
+        
+    // }
+        
     
 }
