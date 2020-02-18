7f1a9160288c5695b9a1b83c35bfd51a56983bed
MugenMan <powerheller@gmail.com>
2020-02-18 23:30:28 +0500

    edit44

--- 44.cpp
+++ 44.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int main(){
-    int number, three, one, ten, hundred;
+    int number, three, one, ten, hudred, order, c=0;
     std::string ones[]={"","one","two","three", "four", "five", "six", "seven", "eight", "nine"};
     std::string tens[]={"", "", "twenty", "thirty", "forty", "fivty", "sixty", "seventy", "eighty", "ninty"};
     std::string teens[]={"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
@@ -20,9 +20,11 @@ int main(){
         number = number / 1000;
         one = three % 10;
         ten = three % 100/10;
-        hundred = three / 100;
+        hudred = three / 100;
+        order = three %1000/10;
         a = " ";
-
+        
+        c++;
         if (ten == 1){
             a = teens[one];
         }
@@ -31,16 +33,19 @@ int main(){
         }
         
 
-        if (hundred != 0){
-            a = ones[hundred] + " hundred " + a;
+        if (hudred != 0){
+            a = ones[hudred] + " hundred " + a;
+            r = a +orders[c] + r;
         }
-        r = a + orders[number] + r;
+        //a = ones[hundred] + " hundred " + a;
+        
         std::cout << "one= " << one <<std::endl;
         std::cout << "ten= " << ten <<std::endl;
-        std::cout << "hundred= " << hundred <<std::endl;
+        std::cout << "hudred= " << hudred <<std::endl;
+        std::cout << "order= " << order <<std::endl;
         std::cout << "a= " << a <<std::endl;
         std::cout << "r= " << r <<std::endl;
-        //a = ones[hundred] + " hundred " + a;
+        //
         //r = buffer + orders[order]+r;
     }
 }
