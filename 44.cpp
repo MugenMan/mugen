13572ee210295ba9b3c6a0dcc132e0aa098ce805
boltPC1 <info@schoolbolt.ru>
2020-02-19 20:29:19 +0500

    edit44

--- 44.cpp
+++ 44.cpp
@@ -1,50 +1,70 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
 
 int main(){
-    int number, three, one, ten, hudred, order, c=0;
+    int number, three, one, ten, hudred, order=0;
     std::string ones[]={"","one","two","three", "four", "five", "six", "seven", "eight", "nine"};
     std::string tens[]={"", "", "twenty", "thirty", "forty", "fivty", "sixty", "seventy", "eighty", "ninty"};
     std::string teens[]={"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     std::string orders[]={"", "thousand", "million", "billion"};
-    std::string a;
-    std::string r;
-    std::string a3;
+    std::string buffer;
+    std::string result;
+    
+
     std::cout << "Enter string: " << std::flush;
     std::cin>> number;
 
-    
+    if (number == 0){
+        std::cout<< "zero" <<std::endl;
+        
+    }
     while (number !=0){
+        
         three = number%1000;
         number = number / 1000;
         one = three % 10;
         ten = three % 100/10;
         hudred = three / 100;
-        order = three %1000/10;
-        a = " ";
         
-        c++;
+        buffer = "";
+        
+        order++; //кол-во троек/текущая тройка
+
         if (ten == 1){
-            a = teens[one];
+            buffer = teens[one];
         }
         else {
-            a = tens[ten]+" " + ones[one];
+            buffer = tens[ten]+" "+ones[one];
         }
-        
 
-        if (hudred != 0){
-            a = ones[hudred] + " hundred " + a;
-            r = a +orders[c] + r;
+
+        std::cout << "one= " << one <<std::endl;
+        std::cout << "ten= " << ten <<std::endl;
+        std::cout << "number= " << number <<std::endl;
+        std::cout << "hudred= " << hudred <<std::endl;
+        std::cout << "buffer= " << buffer <<std::endl;
+
+    if (hudred == 0){
+        buffer = ones[hudred]+" " + buffer;
+    }
+    else {
+        buffer = " "+ones[hudred] + " hundred " + buffer;
+    }
+       
+        if (number != 0){
+            
+            buffer = ones[number] +" " + orders[order]+buffer;
+            
+                result = buffer +" "+orders[order] + result;
+             
+            
         }
         //a = ones[hundred] + " hundred " + a;
         
-        std::cout << "one= " << one <<std::endl;
-        std::cout << "ten= " << ten <<std::endl;
-        std::cout << "hudred= " << hudred <<std::endl;
+        
         std::cout << "order= " << order <<std::endl;
-        std::cout << "a= " << a <<std::endl;
-        std::cout << "r= " << r <<std::endl;
+        std::cout << "buffer= " << buffer <<std::endl;
+        std::cout << "result= " << result <<std::endl;
         //
         //r = buffer + orders[order]+r;
     }
