164fe5de742fd4c172bb8743fb01b4072d6feb65
boltPC1 <info@schoolbolt.ru>
2020-03-02 20:32:15 +0500

    add43v1

--- 43_упорядоченныймассив.cpp
+++ 43_упорядоченныймассив.cpp
@@ -5,7 +5,7 @@
 
 int main(){
     srand(time(NULL));
-    int teams, games, bestpoints = 0, bestteams = 0;
+    int teams, games, bestpoints = 0, bestteams = 0, itemsi, itemsj;
 
     // std::cout << "Enter the number of teams and games: " << std::flush;
     // std::cin>> teams >> games;
@@ -25,14 +25,27 @@ int main(){
         for (int j = 0; j < games; j++){
             row.push_back(rand()%(10-1+1)+1);
         }
+        for (int i = 0; i < teams; i++){
+            std::cout << i<< "= " << row[i] <<" "<<std::endl;
+        }
         arr.push_back(row);
+        arr1.push_back(row);
     }
+    
+
+    
+
+
+
+
 
     for (int i = 0; i < teams; i++){
         int sum = 0;
         std::cout << "Teams "<< i+1 << ": " << std::flush;
         for (int j = 0; j < games; j++){
             sum += arr[i][j];
+            //itemsi = i;
+            //row1.push_back(i);
             std::cout << arr[i][j] << " " << std::flush;
         }
         std::cout << "| Total points: " << sum << std::flush;
@@ -49,5 +62,38 @@ int main(){
     }
     std::cout<<std::endl;
     std::cout <<"Winner team: "<< bestteams + 1 << " | Total points: " << bestpoints << std::endl;
+    
+    
+    int a, b;
+    for (int i = 0; i < teams; i++){
+        int sum = 0;
+        std::cout << "Teams "<< i+1 << ": " << std::flush;
+        for (int j = 0; j < games; j++){
+            sum += arr1[i][j];
+            //itemsi = i;
+            //row1.push_back(i);
+            a = arr1[i][j];
+            b = i-1;
+            while ((arr1[b][j] > a) && (b >= 0)){
+                arr1[b+1][j] = arr1[b][j];
+                arr1[b][j] = a;
+                b--;
+            }
+        
+            std::cout << arr1[i][j] << " " << std::flush;
+        }
+        std::cout << "| Total points: " << sum << std::flush;
+        if (bestpoints < sum){
+             bestpoints = sum;
+             bestteams = i;
+         }
+         if (bestpoints == sum){
+             if (bestteams > i){
+                 bestteams = i;
+             }
+         }
+         std::cout<<std::endl;
+    }
+    
 
 }
