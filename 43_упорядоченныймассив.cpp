0a74584e6c03907f304804ae3206fc701d267ae4
MugenMan <powerheller@gmail.com>
2020-03-03 17:25:57 +0500

    edit43v1

--- 43_упорядоченныймассив.cpp
+++ 43_упорядоченныймассив.cpp
@@ -16,7 +16,7 @@ int main(){
     std::cout << std::endl;
 
     std::vector <std::vector <int> > arr1;
-    std::vector <int> row1;
+   
 
     std::vector <std::vector <int> > arr;
 
@@ -24,21 +24,14 @@ int main(){
         std::vector <int> row;
         for (int j = 0; j < games; j++){
             row.push_back(rand()%(10-1+1)+1);
-        }
-        for (int i = 0; i < teams; i++){
-            std::cout << i<< "= " << row[i] <<" "<<std::endl;
+            //row1=row;
         }
         arr.push_back(row);
         arr1.push_back(row);
+       //std::cout << "arr= "<< row[i] << ": " << std::flush;
     }
+    //std::cout << "arr= "<< row[0] << ": " << std::flush;
     
-
-    
-
-
-
-
-
     for (int i = 0; i < teams; i++){
         int sum = 0;
         std::cout << "Teams "<< i+1 << ": " << std::flush;
@@ -63,37 +56,41 @@ int main(){
     std::cout<<std::endl;
     std::cout <<"Winner team: "<< bestteams + 1 << " | Total points: " << bestpoints << std::endl;
     
-    
-    int a, b;
-    for (int i = 0; i < teams; i++){
-        int sum = 0;
-        std::cout << "Teams "<< i+1 << ": " << std::flush;
-        for (int j = 0; j < games; j++){
-            sum += arr1[i][j];
-            //itemsi = i;
-            //row1.push_back(i);
-            a = arr1[i][j];
-            b = i-1;
-            while ((arr1[b][j] > a) && (b >= 0)){
-                arr1[b+1][j] = arr1[b][j];
-                arr1[b][j] = a;
-                b--;
+    long buf_mus;int **a,i, j, nrow, ncol, b, buf_a;
+    double *mus = new double [teams];
+         for (int i = 0;i < teams;i++)
+        { mus[i] = 0;
+          for (int j = 0;j < games; j++)
+         mus[i] += arr[i][j];
+         }
+    for(int i = 0;i < teams-1; i++)
+           {if (sum[i]==1)
+                {b = i;
+             for(int j = i+1;j < teams; j++)
+                 if (mus[j] > mus[b])
+                     b = j;
+                     buf_mus = mus[i];
+                     mus[i] = mus[b];
+                     mus[b] = buf_mus;
+                      for(int j = 0;j < games;j++)
+                         { buf_a = arr[i][j];
+                            arr[i][j] = arr[b][j];
+                            arr[b][j] = buf_a;
+                         }
+                }
             }
         
-            std::cout << arr1[i][j] << " " << std::flush;
+    
+    
+    std::cout<<"new :"<<std::endl;
+    for (int i = 0; i < teams; i++){
+        int sum = 0;
+        for (int j = 0; j < games; j++){
+            sum += arr[i][j];
+            std::cout << arr[i][j] << " " << std::flush;
+            
         }
         std::cout << "| Total points: " << sum << std::flush;
-        if (bestpoints < sum){
-             bestpoints = sum;
-             bestteams = i;
-         }
-         if (bestpoints == sum){
-             if (bestteams > i){
-                 bestteams = i;
-             }
-         }
-         std::cout<<std::endl;
+        std::cout<<std::endl;
     }
-    
-
 }
