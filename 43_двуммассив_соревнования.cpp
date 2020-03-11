250e79540aa1d22d2ac456611a62eea177262198
boltPC1 <info@schoolbolt.ru>
2020-03-11 19:01:52 +0500

    edit44_x&0

--- 43_двуммассив_соревнования.cpp
+++ 43_двуммассив_соревнования.cpp
@@ -15,27 +15,29 @@ int main(){
     std::cin >> games;
     std::cout << std::endl;
 
-    std::vector <std::vector <int> > arr1;
-    std::vector <int> row1;
-
-    std::vector <std::vector <int> > arr;
-
+    std::vector <std::vector <int> > arr;//основной
+    std::vector <std::vector <int> > new_arr;//вспомогательный
+   
     for (int i = 0; i < teams; i++){
-        std::vector <int> row;
+        std::vector <int> row;//вспомог
         for (int j = 0; j < games; j++){
             row.push_back(rand()%(10-1+1)+1);
         }
         arr.push_back(row);
     }
 
+    
     for (int i = 0; i < teams; i++){
         int sum = 0;
         std::cout << "Teams "<< i+1 << ": " << std::flush;
         for (int j = 0; j < games; j++){
+
             sum += arr[i][j];
             std::cout << arr[i][j] << " " << std::flush;
         }
+
         std::cout << "| Total points: " << sum << std::flush;
+
         if (bestpoints < sum){
              bestpoints = sum;
              bestteams = i;
@@ -49,5 +51,53 @@ int main(){
     }
     std::cout<<std::endl;
     std::cout <<"Winner team: "<< bestteams + 1 << " | Total points: " << bestpoints << std::endl;
+
+
+    for (int i = 0; i < teams; i++){
+        std::vector <int> row1;//вспомог
+        for (int j = 0; j < 2; j++){
+            row1.push_back(0);
+        }
+        new_arr.push_back(row1);
+    }
+
+    for (int i = 0; i < teams; i++){
+        new_arr[i][0] = i;
+        for (int j = 0; j < games; j++){
+            new_arr[i][1]+= arr[i][j];
+        }
+    }
+    std::cout<<std::endl;
+
+    for (int i = 0; i < teams-1; i++){
+        for (int k = i+1; k < teams; k++){
+            if (new_arr[i][1] < new_arr[k][1]){
+
+                // std::vector <int> tmp = arr[i];
+                // arr[i] = arr[k];
+                // arr[k] = tmp;
+
+                std::vector <int> counter = new_arr[i];
+                new_arr[i] = new_arr[k];
+                new_arr[k] = counter;
+                
+            }
+        }
+    }
+
+    std::cout<<"ordered array"<<std::endl;
+
+    for (int i = 0; i < teams; i++){
+        //int sum = 0;
+        std::cout << "Teams "<< new_arr[i][0]+1 << ": " << std::flush;
+        for (int j = 0; j < games; j++){
+            //std::cout << arr[i][j] << " " << std::flush;
+            std::cout << arr[new_arr[i][0]][j] << " " << std::flush;
+            
+        }
+        std::cout << "| Total points: " << new_arr[i][1] << std::flush;
+        std::cout<<std::endl;
+    }
+    std::cout<<std::endl;
 
 }
---
+++ 44_x&0.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(){
+    char x, y;
+    std::vector <std::vector <char> > arr;
+
+    //  for (int i = 0; i < 3; i++){
+    //     std::vector <char> row;//вспомог
+    //     for (int j = 0; j < 3; j++){
+    //         row.push_back("d");
+    //     }
+    //     arr.push_back(row);
+    // }
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            std::cout<<"+"<<std::flush;
+            if (j < 3){
+                std::cout<<"-"<<std::flush;
+            }
+        }
+        std::cout<<std::endl;
+        for (int j = 0; j < 4; j++){
+            if (i == 3){
+                break;
+            } 
+            std::cout<<"|"<<std::flush;
+            //std::cout<<"o"<<std::flush;
+            if (j < 3){
+                std::cout<<"o"<<std::flush;
+            }
+            
+        }
+        
+        std::cout<<std::endl;
+    }
+
+    while (arr[0][0] != "0"){
+        std::cout <<"Turn: "<< std::endl;
+        std::cout <<"Enter cell number:"<< std::endl;
+        std::cout <<"x: "<< std::flush;
+        std::cin >> x;
+        std::cout <<"y: "<< std::flush;
+        std::cin >> y;
+    }
+    
+}
